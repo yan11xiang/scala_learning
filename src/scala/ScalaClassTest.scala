a238e86731352efa61c7b0f3ab25842d0f20ba46
yan11xiang <yanxiang60@gmail.com>
2017-05-07 21:55:22 +0800

    test

---
+++ src/scala/ScalaClassTest.scala
@@ -0,0 +1,17 @@
+package scala
+
+/**
+  * @author 小C
+  * @since 2017/5/7
+  */
+class ScalaClassTest {
+
+  def main(args: Array[String]): Unit = {
+    print("Scala Class Test")
+    print(test())
+  }
+
+  def test() : String = {
+    return "test()"
+  }
+}
--- src/scala/ScalaTest.scala
+++ src/scala/ScalaTest.scala
@@ -1,30 +1,34 @@
 package scala
 
 /**
-  * @author 小C
+  * @author CBrother
   * @since 2017/5/7
   */
 object ScalaTest {
 
   def main(args: Array[String]) {
-//    var randSent = "I saw a dragon fly by"
-//    println("String length :" + randSent.length)
-//
-//    println(randSent.concat("saw "))
-//
-//    val randSentArray = randSent.toArray
-//
-//    for(v <- randSentArray) {
-//      println(v)
-//    }
 
+    //字符串的处理
+    var randSent = "I saw a dragon fly by"
+
+    println("String length :" + randSent.length)
+    println(randSent.concat("saw "))
+    val randSentArray = randSent.toArray
+    //for循环
+    for(v <- randSentArray) {
+      println(v)
+    }
+
+    //构建方法 def 方法名(参数名 : 参数类型 = 默认参数值, ...) : 返回值（Unit = Java中的void） = { 方法体}
     def getSum(num1 : Int = 1, num2 : Int = 2) : Int = {
       num1 + num2
     }
-
     println(" 5 + 4 = " + getSum(5, 4))
 
+    //var 隐式转换， 实际类型为 ScalaClassTest
+    var a = new ScalaClassTest()
+    // 调用Java类库
+    System.out.print(a.test())
 
-    
   }
 }
